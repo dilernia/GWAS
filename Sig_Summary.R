9cbef090005f4ffd188e5c24fd19b16ceedac1e5
Andrew DiLernia <andrew.s.dilernia@gmail.com>
2019-04-21 22:44:03 -0500

    Update Sig_Summary.R
    (1) Added code for calculating R^2 values for predicting phenotypes with corresponding significant SNPs

--- Sig_Summary.R
+++ Sig_Summary.R
@@ -9,57 +9,112 @@ library(tidyverse)
 library(foreach)
 library(doParallel)
 library(data.table)
+library(broom)
 library(xlsx)
 library(rsnps)
 library(qqman)
 
 # SNPs for MCM10 and MGMT were obtained here: https://www.ncbi.nlm.nih.gov/guide/howto/view-all-snps/
-Datdir <- "/home/andrew/andrew.old/Gdrive_Backup/Tobacco_Research/New/"
 
-# Reading in GWAS results and only selecting significant snps
-readSig <- function(p, population = "CEU", snps = sigSNPs) {
-  temp <- readRDS(paste0(Datdir, "Analysis_Results/", population, "_Mass_Uni_Results_", p, ".rds"))
-  
-  #Initialize empty data table
-  combined <- data.table(
-    snp = character(),
-    tVal = numeric(),
-    pVal = numeric(),
-    phenotype = character(),
-    pop = character())
-  
-  for(i in 1:length(temp)) {
-    results <- data.table(snp = names(temp[[i]]$tVal_pVal), 
-                          tVal = unlist(temp[[i]]$tVal_pVal)[seq(1, length(unlist(temp[[i]]$tVal_pVal)), by = 2)], 
-                          pVal = unlist(temp[[i]]$tVal_pVal)[seq(2, length(unlist(temp[[i]]$tVal_pVal)), by = 2)],
-                          phenotype = temp[[i]]$phenotype,
-                          pop = population)
-    combined <- rbindlist(list(combined, results[snp %in% snps]))
+if(file.exists("sigSNPres.rds") == FALSE) {
+  # Reading in GWAS results and only selecting significant snps
+  readSig <- function(p, population = "CEU", snps = sigSNPs) {
+    temp <- readRDS(paste0("Analysis_Results/", population, "_Mass_Uni_Results_", p, ".rds"))
+    
+    #Initialize empty data table
+    combined <- data.table(
+      snp = character(),
+      tVal = numeric(),
+      pVal = numeric(),
+      phenotype = character(),
+      pop = character())
+    
+    for(i in 1:length(temp)) {
+      results <- data.table(snp = names(temp[[i]]$tVal_pVal), 
+                            tVal = unlist(temp[[i]]$tVal_pVal)[seq(1, length(unlist(temp[[i]]$tVal_pVal)), by = 2)], 
+                            pVal = unlist(temp[[i]]$tVal_pVal)[seq(2, length(unlist(temp[[i]]$tVal_pVal)), by = 2)],
+                            phenotype = temp[[i]]$phenotype,
+                            pop = population)
+      combined <- rbindlist(list(combined, results[snp %in% snps]))
+    }
+    print(p)
+    return(combined)
   }
-  print(p)
-  return(combined)
+  
+  # Determining significant SNPs
+  sigSNPs <- bind_rows(readRDS("Analysis_Results/CEU_Combined_Results_Sig.rds"), 
+                       readRDS("Analysis_Results/YRI_Combined_Results_Sig.rds")) %>% 
+    select(snp) %>% unlist() %>% unique()
+  
+  inputs <- expand.grid(p = 1:20, population = c("CEU", "YRI"), stringsAsFactors = FALSE)
+  
+  sigSNPres <- pmap_dfr(.f = readSig, .l = list(p = inputs$p, population = inputs$population))
+  sigSNPres <- sigSNPres %>% filter(!is.na(tVal) & phenotype != "Viability") %>% 
+    mutate(phenotype = gsub(x = phenotype, pattern = "Avg_", replacement = ""))
+  
+  # Saving significant SNP results
+  saveRDS(sigSNPres, "sigSNPres.rds")
+}
+sigSNPres <- readRDS("sigSNPres.rds")
+
+# Exploring variation explained by significant SNPs -----------------------
+
+# Determining significant SNP's and phenotypes
+snpPheno <- sigSNPres %>% filter(pVal <= 5*10^(-8))
+remove("sigSNPres")
+
+# Importing cleaned phenotype data
+dir <- "/home/andrew/andrew.old/Gdrive_Backup/Tobacco_Research/"
+phenData <- rbind(readRDS(paste0(dir, "CEU_Pheno_Clean.rds")), 
+                  readRDS(paste0(dir, "YRI_Pheno_Clean.rds")))
+colnames(phenData) <- colnames(phenData) %>% gsub(pattern = "Avg_", replacement = "")
+phenData <- phenData[, c("Cell_line", "Ethnicity", "Gender", "turnover", unique(snpPheno$phenotype))]
+
+# Importing cleaned genotype data
+if(sum(c(file.exists("sigCEUGenDat.rds"), file.exists("sigYRIGenDat.rds"))) < 2) {
+ceuGenDat <- as.data.frame(setDT(readRDS("CEU_Gen_Dat_Clean2.Rda"))[, 
+                           c("Cell_line", snpPheno[which(snpPheno$pop == "CEU"), c("snp")]), 
+                           with = FALSE])
+ceuGenDat <- ceuGenDat[, !duplicated(colnames(ceuGenDat))]
+saveRDS(ceuGenDat, "sigCEUGenDat.rds")
+yriGenDat <- as.data.frame(setDT(readRDS("YRI_Gen_Dat_Clean2.Rda"))[, 
+                           c("Cell_line", snpPheno[which(snpPheno$pop == "YRI"), c("snp")]), 
+                           with = FALSE])
+yriGenDat <- yriGenDat[, !duplicated(colnames(yriGenDat))]
+saveRDS(yriGenDat, "sigYRIGenDat.rds")
 }
 
-# Determining significant SNPs
-sigSNPs <- bind_rows(readRDS(paste0(Datdir, "Analysis_Results/CEU_Combined_Results_Sig.rds")), 
-                     readRDS(paste0(Datdir, "Analysis_Results/YRI_Combined_Results_Sig.rds"))) %>% 
-  select(snp) %>% unlist() %>% unique()
+fullData <- phenData %>% left_join(readRDS("sigCEUGenDat.rds"), by = c("Cell_line" = "Cell_line")) %>% 
+  left_join(readRDS("sigYRIGenDat.rds"), by = c("Cell_line" = "Cell_line"))
 
-inputs <- expand.grid(p = 1:20, population = c("CEU", "YRI"), stringsAsFactors = FALSE)
+popPhen <- snpPheno[!duplicated(snpPheno[, c("pop", "phenotype")]), c("pop", "phenotype")]
 
-sigSNPres <- pmap_dfr(.f = readSig, .l = list(p = inputs$p, population = inputs$population))
-sigSNPres <- sigSNPres %>% filter(!is.na(tVal) & phenotype != "Viability") %>% 
-  mutate(phenotype = gsub(x = phenotype, pattern = "Avg_", replacement = ""))
+p <- 1
 
-# Saving significant SNP results
-saveRDS(sigSNPres, "sigSNPres.rds")
+# Function for summarize R^2 for each set of significant SNPs
+rSqrSummary <- function(p) {
+pop <- popPhen[p, "pop"]
+pheno <- popPhen[p, "phenotype"]
+sigSnps <- snpPheno[which(snpPheno$pop == pop & snpPheno$phenotype == pheno), "snp"][1]
+phenSub <- fullData[which(fullData$Ethnicity == pop), c(pheno, "Gender", "turnover", sigSnps)]
+phenSub[, sigSnps] <- as.numeric(phenSub[, sigSnps])
+
+fit <- lm(paste(pheno, " ~ Gender + turnover"), data = phenSub[complete.cases(phenSub), ])
+fitSNPs <- lm(paste(pheno, " ~ ."), data = phenSub[complete.cases(phenSub), ])
+return(data.frame(Population = pop, Phenotype = pheno, SNPs = paste(sigSnps, collapse = ", "), 
+           R2 = round(summary(fitSNPs)$r.squared - summary(fit)$r.squared, 4),
+           R2_Gend_Turn = round(summary(fit)$r.squared, 4), 
+           All_SNPs = paste(snpPheno[which(snpPheno$pop == pop & snpPheno$phenotype == pheno), "snp"], collapse = ", ")))
+}
+
+aggRes <- map_dfr(.f = rSqrSummary, .x = 1:nrow(popPhen))
 
 # Gene specific summaries -------------------------------------------------
 
 # Function for determining MAF's for sample
 mafCalc <- function(pop, snps) {
   #Loading new genotype data to obtain chromosome and position data for snps
-  Gen_Dat <- readRDS(paste0(Datdir, pop, "_Gen_Dat_Clean2.Rda"))
+  Gen_Dat <- readRDS(paste0(pop, "_Gen_Dat_Clean2.Rda"))
   
   Gen_Dat <- Gen_Dat[, colnames(Gen_Dat)[which(colnames(Gen_Dat) %in% snps)], with=FALSE]
   
@@ -103,17 +158,17 @@ phenotypes <- sigSNPres$phenotype %>% unique()
 
 # Overall results
 for(p in phenotypes) {
-write.xlsx(sigSNPres %>% filter(phenotype == p) %>% arrange(snp), paste0(Datdir, p, "_results.xlsx"), row.names = FALSE)
+  write.xlsx(sigSNPres %>% filter(phenotype == p) %>% arrange(snp), paste0(p, "_results.xlsx"), row.names = FALSE)
 }
 
 # MGMT results
 for(p in phenotypes) {
-  write.xlsx(mgmtSummary %>% filter(phenotype == p) %>% arrange(SNP), paste0(Datdir, "MGMT_", p, "_results.xlsx"), row.names = FALSE)
+  write.xlsx(mgmtSummary %>% filter(phenotype == p) %>% arrange(SNP), paste0("MGMT_", p, "_results.xlsx"), row.names = FALSE)
 }
 
 # MCM10 results
 for(p in phenotypes) {
-  write.xlsx(mcm10Summary %>% filter(phenotype == p) %>% arrange(SNP), paste0(Datdir, "MCM10_", p, "_results.xlsx"), row.names = FALSE)
+  write.xlsx(mcm10Summary %>% filter(phenotype == p) %>% arrange(SNP), paste0("MCM10_", p, "_results.xlsx"), row.names = FALSE)
 }
 
 # Obtaining SNP positions -------------------------------------------------
@@ -140,17 +195,17 @@ bpFind <- function(snps) {
 
 mcm10Positions <- bpFind(mcm10snps)
 
-write.xlsx(mcm10Positions, paste0(Datdir, "mcm10Positions.xlsx"), row.names = FALSE)
+write.xlsx(mcm10Positions, paste0("mcm10Positions.xlsx"), row.names = FALSE)
 
 mgmtPositions <- bpFind(mgmtsnps)
 
-write.xlsx(mgmtPositions, paste0(Datdir, "mgmtPositions.xlsx"), row.names = FALSE)
+write.xlsx(mgmtPositions, paste0("mgmtPositions.xlsx"), row.names = FALSE)
 
 
 # Manhattan and QQ Plots --------------------------------------------------
 
 # Reading in Excel file
-sigResults <- read.xlsx(paste0(Datdir, "resultsSummary.xlsx"), 1)
+sigResults <- read.xlsx(paste0("resultsSummary.xlsx"), 1)
 Combined_Results <- readRDS(paste0(pop, "_Combined_Results_Sig.rds"))
 
 outcomes <- Combined_Results$phenotype %>% unique()
