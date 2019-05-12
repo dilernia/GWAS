3a74e0bdf12c5553d9d3107c8ab26b39de5ed19f
Andrew DiLernia <andrew.s.dilernia@gmail.com>
2019-05-12 12:45:27 -0500

    Update GWAS_Analysis_MSI.R
    (1) Updated code to not run in parallel due to issues with data.table and foreach

--- GWAS_Analysis_MSI.R
+++ GWAS_Analysis_MSI.R
@@ -12,16 +12,16 @@ library(tidyverse)
 library(stringr)
 library(tidyr)
 library(dplyr)
-library("foreach")
-library("doParallel")
+library(foreach)
+library(doParallel)
 
 dir <- "/panfs/roc/groups/15/petersla/diler001/Genetics/"
-#dir <- "C:/Users/Owner/Google Drive/Genetics_Research/"
+#dir <- "C:/Users/Owner/Google Drive/Genetics_Research/New/"
 
 path <- dir
 
 setwd(dir)
-population <- "CEU"
+population <- "YRI"
 
 cleaned <- FALSE
 
@@ -30,14 +30,13 @@ cleaned <- FALSE
 if(file.exists("CEU_Gen_Dat_Check.csv") == FALSE) {
   load("file_names.Rda")
   
-  file_names <- c(file_names[which(grepl("CEU", file_names, fixed=TRUE))],
-                  file_names[which(grepl("YRI", file_names, fixed=TRUE))])
+  file_names <- c(file_names[which(grepl(population, file_names, fixed=TRUE))])
   
   URL <- "ftp://ftp.ncbi.nlm.nih.gov/hapmap/genotypes/2010-08_phaseII+III/forward/"
   
   URLs = paste0(URL, file_names)
   
-  Gen_Get <- function(index, urls = URLs, pop = "CEU")
+  Gen_Get <- function(index, urls = URLs, pop)
   {
     files <- file_names[which(grepl(pop, file_names, fixed=TRUE))]
     
@@ -49,146 +48,200 @@ if(file.exists("CEU_Gen_Dat_Check.csv") == FALSE) {
     fread(substr(x = files[[index]], start=0, stop = (nchar(files[[index]]) - 3)), fill = TRUE)
   }
   
-  CEU_Gen_Dat <- map_dfr(.f = Gen_Get, .x = as.list(1:25),
-                         pop = "CEU")
-  YRI_Gen_Dat <- map_dfr(.f = Gen_Get, .x = as.list(1:25),
-                         pop = "YRI")
+  Gen_Dat <- rbindlist(lapply(FUN = Gen_Get, 
+                              X = 1:length(file_names), pop = population))
   
   # Subsetting by columns
-  CEU_Gen_Dat <- setDT(CEU_Gen_Dat)[, c("chrom", "pos", "strand", "assembly#", "center",
-                                        "protLSID", "assayLSID", "panelLSID", "QCcode") := NULL]
-  YRI_Gen_Dat <- setDT(YRI_Gen_Dat)[, c("chrom", "pos", "strand", "assembly#", "center",
-                                        "protLSID", "assayLSID", "panelLSID", "QCcode") := NULL]
+  Gen_Dat <- Gen_Dat[, c("chrom", "pos", "strand", "assembly#", "center",
+                         "protLSID", "assayLSID", "panelLSID", "QCcode") := NULL]
   
-  fwrite(CEU_Gen_Dat, file = "CEU_Gen_Dat_Check.csv")
-  fwrite(YRI_Gen_Dat, file = "YRI_Gen_Dat_Check.csv")
+  fwrite(Gen_Dat, file = paste0(population, "_Gen_Dat_Check.csv"))
 }
 
 #####End initial downloading of data#####
-
-#####Loading old SNP genotypes#####
-
-if(file.exists(paste0("snp_matrix.", population, "_Check.csv")) == FALSE) {
-  SNP_data <- fread(paste0(path,'snp_matrix.', population, '.csv'))
-  
-  # Cleans up snp names
-  colnames(SNP_data) <- colnames(SNP_data) %>%
-    gsub(pattern = " ", replacement = "") %>% tolower()
-  
-  # Only keeps columns with valid snp names
-  SNP_data <- SNP_data %>% as.data.frame()
-  SNP_data <- SNP_data[, c(1, which(str_detect(colnames(SNP_data), "rs")))]
-  
-  colnames(SNP_data)[1] <- "Cell_line"
-  
-  Cell_lines <- SNP_data$Cell_line %>% gsub(pattern = " ", replacement = "") %>% toupper()
-  
-  # Adds back in Cell_line column
-  SNP_data$Cell_line <- unlist(Cell_lines)
-  print(paste0("Number of cell lines: ", length(Cell_lines)))
-  
-  # Saves original data
-  fwrite(SNP_data, file = paste0("snp_matrix.", population, "_Check.csv"))
-}
 
 # Obtaining vector of population cell lines
 load(paste0("Original_Data/", population, "_IC_data.Rda"))
 Cell_lines <- get(paste0(population, "_IC_data"))$Cell_line
 remove(list = paste0(population, "_IC_data"))
 
-# Obtaining vector of original snps
-SNP_data <- fread(paste0("snp_matrix.", population, "_Check.csv"))
-remove("SNP_data")
 print(paste0("Number of cell lines: ", length(Cell_lines)))
 
 if(cleaned == FALSE) {
   
   #####Cleaning new genotype data#####
   
-  numRows <- nrow(fread(paste0(population, "_Gen_Dat_Check.csv"), select = 1))
-  parts <- 100
+  numRows <- nrow(fread(paste0(population, "_Gen_Dat_Check.csv"), select = 1)) + 1
+  parts <- 1000
   chunkSize <- ceiling(numRows/parts)
+  lastSize <- chunkSize*parts - numRows
   
-  stopStart <- data.frame(start = seq(1, numRows, by = chunkSize), 
-                          stop = c((seq(1, numRows, by = chunkSize)-1)[-1], numRows))
+  genColNames <- colnames(fread(paste0(population, "_Gen_Dat_Check.csv"), 
+                                 header = T, nrows = 0))
 
-  library(foreach)
-  library(doParallel)
-  ncores <- 10 # set ncores to something less than or equal to available cores
+  for(p in 1:parts) {
+  
+    # Loading partition of new genotype data
+    Gen_Dat <- fread(paste0(population, "_Gen_Dat_Check.csv"), 
+                     header = (p==1), skip = (p-1)*chunkSize, 
+                     nrows = (p==parts)*lastSize + (p!=parts)*chunkSize)
+    colnames(Gen_Dat) <- genColNames
+    
+    # Loading snps to be excluded due to potential errors
+    SNPs_to_Exclude <- read_excel(paste0(path, "SNPs_to_Exclude.xlsx"))$rsid %>% unique() %>% 
+      gsub(pattern = " ", replacement = "") %>% tolower()
+    
+    # Cleaning up snp names
+    colnames(Gen_Dat)[which(colnames(Gen_Dat) == "rs#")] <- "SNP" 
+    Gen_Dat$SNP <- Gen_Dat$SNP %>% gsub(pattern = " ", replacement = "") %>% tolower()
+    
+    # Cleaning up cell line names
+    colnames(Gen_Dat) <- colnames(Gen_Dat) %>% gsub(pattern = " ", replacement = "") %>% toupper()
+    
+    # Subsetting to only keep necessary cell line columns
+    Cell_lines <- Cell_lines %>% unlist() %>% trimws() %>% toupper()
+    Gen_Dat <- Gen_Dat[, c(1:2, which(colnames(Gen_Dat) %in% Cell_lines)), with = FALSE]
+    
+    # Determining which snps are not in error list of snps
+    # where errors are from: ftp://ftp.ncbi.nlm.nih.gov/hapmap/genotypes/2010-08_phaseII+III/inconsistencies/
+    SNPs_ok <- Gen_Dat$SNP[which(!(Gen_Dat$SNP %in% c(SNPs_to_Exclude)))] %>% unique() 
+    
+    # Excludes error snps from: ftp://ftp.ncbi.nlm.nih.gov/hapmap/genotypes/2010-08_phaseII+III/inconsistencies/
+    Gen_Dat <- as.data.frame(Gen_Dat[SNP %in% SNPs_ok])
+    
+    #####Recoding SNP values in new data#####
+    
+    # Creating vector of valid genotypes
+    genotypes <- apply(expand.grid(c("A", "T", "G", "C"), c("A", "T", "G", "C")), 
+                       1, FUN = paste, collapse = "")
+    
+    # Setting values not a valid genotype to missing
+    temp <- Gen_Dat[, c(1:2)]
+    Gen_Dat[Gen_Dat != genotypes[1] & Gen_Dat != genotypes[2] & Gen_Dat != genotypes[3] &
+              Gen_Dat != genotypes[4] & Gen_Dat != genotypes[5] & Gen_Dat != genotypes[6] &
+              Gen_Dat != genotypes[7] & Gen_Dat != genotypes[8] & Gen_Dat != genotypes[9] & 
+              Gen_Dat != genotypes[10] & Gen_Dat != genotypes[11] & Gen_Dat != genotypes[12] &
+              Gen_Dat != genotypes[13] & Gen_Dat != genotypes[14] & Gen_Dat != genotypes[15] & 
+              Gen_Dat != genotypes[16]] <- NA 
+    
+    Gen_Dat[, c(1:2)] <- temp
+    remove(temp)
+    
+    # Creating function for converting from string genotype to MAF
+    Converter <- function(column, key = Gen_Dat[, 2]) {
+      for(r in 1:length(column)) {
+        if (!is.na(column[r])){
+          # Converts from string genotype to MAF
+          column[r] <- str_count(str_sub(column[r], 1, 1), 
+                                 str_sub(key[r], -1, -1)) + 
+            str_count(str_sub(column[r], -1, -1), 
+                      str_sub(key[r], -1, -1))
+        }
+      }
+      return(as.integer(column))
+    }
+    
+    # Converting from genotype to MAF using data.table code
+    Gen_Dat <- setDT(Gen_Dat)
+    Gen_Dat <- Gen_Dat[, (colnames(Gen_Dat)[-c(1:2)]) := lapply(.SD, Converter),
+                       .SDcols = colnames(Gen_Dat)[-c(1:2)]]
+
+    # Saving data
+    fwrite(Gen_Dat, file = paste0("Partitions/", population, "_Gen_Dat_Clean_Check", p, ".csv"))
+    print(paste0(Sys.time(), ", Partition ", p, " complete"))
+  }
+}
+
+# Reading in cleaned partitions and aggregating for analysis
+Gen_Dat <- rbindlist(lapply(X = str_subset(list.files("Partitions/"), pattern = population),
+  FUN = function(x) {
+    return(dcast(melt(fread(paste0("Partitions/", x))[,ALLELES:=NULL], 
+                      id.vars = "SNP"), variable ~ SNP))
+    }))
+
+Gen_Dat <- dcast(melt(Gen_Dat[,ALLELES:=NULL], id.vars = "SNP"), variable ~ SNP)
+colnames(Gen_Dat)[1] <- "Cell_line"
+
+fwrite(Gen_Dat, file = paste0(population, "Gen_Dat_Clean_check.csv"))
+
+#####Reading original phenotype data in#####
+
+phenFiles <- paste0(dir, "Original_Data/", population, "_", 
+                    c("Cyto", "Repair", "IC", "Mut", "Mutation"), "_data.Rda")
+for(f in phenFiles){load(f)}
+
+#Creates list of phenotype data sets
+PhenDats <- lapply(X = paste0(population, "_", 
+                              c("Cyto", "Repair", "IC", "Mut", "Mutation"), "_data"), 
+                   FUN = function(x){eval(parse(text=x))})
+
+#Creating function for cleaning cell line names
+PhenDat_Cleaner <- function(dat) {
+  dat$Cell_line <- dat$Cell_line %>% gsub(pattern = "\\s", replacement = "") %>% toupper()
+  return(dat)
+}
+PhenDats <- lapply(X = PhenDats, FUN = PhenDat_Cleaner)
+
+#Combining all phenotype datasets into one
+PhenData_Combined <- PhenDats %>% 
+  Reduce(function(dtf1,dtf2) full_join(dtf1,dtf2,by="Cell_line"), .) %>% 
+  dplyr::select(-ends_with(".x")) %>% dplyr::select(-ends_with(".y")) %>% 
+  dplyr::select(Cell_line, Ethnicity, Gender, everything())
+
+#Calculating log transformations for IC data, recoding gender variable, log mut diffs
+PhenData_Combined <- PhenData_Combined %>% mutate(Avg_IC20 = log(Avg_IC20), Avg_IC50 = log(Avg_IC50), 
+                                                  Avg_IC80 = log(Avg_IC80), Gender = factor(Gender), 
+                                                  Mut10v0 = log10((Mut10v0+abs(Mut10v0))/2 + 0.01), 
+                                                  Mut20v0 = log10((Mut20v0+abs(Mut20v0))/2 + 0.01),
+                                                  Mut20v10 = log10((Mut20v10+abs(Mut20v10))/2 + 0.01)) %>% 
+  dplyr::select(-c(Mut0, Mut10, Mut20))
+
+#####Looping through doing analysis for each partition of data#####
+for(p in 1:20) {
+  #Loading new genotype data
+  Gen_Dat <- readRDS(paste0(population, "_Gen_Dat_Clean2.Rda"))
+  Gen_Dat <- setDT(Gen_Dat)
+  
+  #Separating genotype data into 20 partitions
+  num_cols <- ncol(Gen_Dat); 
+  indices <- c(seq(1, num_cols, by = ceiling(num_cols/20)), num_cols)
+  indices[1] <- 0
+  Gen_Dat <- Gen_Dat[, (indices[p]+1):indices[p+1]] %>% as.data.frame()
+  
+  #Converting columns to numeric type
+  #Gen_Dat <- Gen_Dat[, (colnames(Gen_Dat)[-c(1)]) := lapply(.SD, as.numeric), .SDcols = colnames(Gen_Dat)[-c(1)]]
+  Gen_Dat[, -c(1)] <- sapply(Gen_Dat[, -c(1)], as.numeric)
+  
+  #Running mass univariate analysis for given genotype data subset
+  detectCores()
+  ncores <- 10 #Set ncores to something less than or equal to available cores
   cl <- makeCluster(ncores) # creates a cluster with <ncore> cores
   registerDoParallel(cl) # register the cluster
   
-  res <- foreach(chunk = 1:nrow(stopStart)) %dopar% {
+  results <- foreach(j = 4:ncol(PhenData_Combined)) %dopar% {
     library(data.table)
-    library(readxl)
     library(tidyverse)
-  
-  # Loading partition of new genotype data
-  Gen_Dat <- fread(paste0(population, "_Gen_Dat_Check.csv"), skip = (chunk-1)*chunkSize, nrows = chunkSize)
-  
-  # Loading snps to be excluded due to potential errors
-  SNPs_to_Exclude <- read_excel(paste0(path, "SNPs_to_Exclude.xlsx"))$rsid %>% unique() %>% 
-    gsub(pattern = " ", replacement = "") %>% tolower()
-  
-  # Cleaning up snp names
-  colnames(Gen_Dat)[which(colnames(Gen_Dat) == "rs#")] <- "SNP" 
-  Gen_Dat$SNP <- Gen_Dat$SNP %>% gsub(pattern = " ", replacement = "") %>% tolower()
-  
-  # Cleaning up cell line names
-  colnames(Gen_Dat) <- colnames(Gen_Dat) %>% gsub(pattern = " ", replacement = "") %>% toupper()
-  
-  # Subsetting to only keep necessary cell line columns
-  Cell_lines <- Cell_lines %>% unlist() %>% trimws() %>% toupper()
-  Gen_Dat <- Gen_Dat[, c(1:2, which(colnames(Gen_Dat) %in% Cell_lines)), with = FALSE]
-  
-  # Determining which snps are not in error list of snps
-  # where errors are from: ftp://ftp.ncbi.nlm.nih.gov/hapmap/genotypes/2010-08_phaseII+III/inconsistencies/
-  SNPs_ok <- Gen_Dat$SNP[which(!(Gen_Dat$SNP %in% c(SNPs_to_Exclude)))] %>% unique() 
-  
-  # Excludes error snps from: ftp://ftp.ncbi.nlm.nih.gov/hapmap/genotypes/2010-08_phaseII+III/inconsistencies/
-  Gen_Dat <- as.data.frame(Gen_Dat[SNP %in% SNPs_ok])
-  
-  #####Recoding SNP values in new data#####
-  
-  # Creating vector of valid genotypes
-  genotypes <- apply(expand.grid(c("A", "T", "G", "C"), c("A", "T", "G", "C")), 
-                     1, FUN = paste, collapse = "")
-  
-  # Setting values not a valid genotype to missing
-  temp <- Gen_Dat[, c(1:2)]
-  Gen_Dat[Gen_Dat != genotypes[1] & Gen_Dat != genotypes[2] & Gen_Dat != genotypes[3] &
-            Gen_Dat != genotypes[4] & Gen_Dat != genotypes[5] & Gen_Dat != genotypes[6] &
-            Gen_Dat != genotypes[7] & Gen_Dat != genotypes[8] & Gen_Dat != genotypes[9] & 
-            Gen_Dat != genotypes[10] & Gen_Dat != genotypes[11] & Gen_Dat != genotypes[12] &
-            Gen_Dat != genotypes[13] & Gen_Dat != genotypes[14] & Gen_Dat != genotypes[15] & 
-            Gen_Dat != genotypes[16]] <- NA 
-  
-  Gen_Dat[, c(1:2)] <- temp
-  remove(temp)
-  
-  # Creating function for converting from string genotype to MAF
-  Converter <- function(column, key = Gen_Dat[, 2]) {
-    for(r in 1:length(column)) {
-      if (!is.na(column[r])){
-        # Converts from string genotype to MAF
-        column[r] <- str_count(str_sub(column[r], 1, 1), 
-                               str_sub(key[r], -1, -1)) + 
-          str_count(str_sub(column[r], -1, -1), 
-                    str_sub(key[r], -1, -1))
-      }
+    #Creating function for fitting MLR model
+    MLR <- function(gendCol, snpCol, phenCol) {
+      temp <- data.frame(g = gendCol, s = snpCol)
+      if(temp[complete.cases(temp), c("g")] %>% unique() %>% length() > 1) {
+        fit <- lm(phenCol ~ gendCol + snpCol)
+        if(dim(summary(fit)$coefficients)[1]>2) {
+          tStat <- summary(fit)$coefficients[3,3]
+          pval <- summary(fit)$coefficients[3,4]
+          return(list(tStat, pval))
+        } else{return(list(NA, NA))}
+      } else{return(list(NA, NA))}
     }
-    return(as.integer(column))
+    
+    tempResult <- lapply(FUN = MLR, X = as.data.frame(Gen_Dat)[, -c(1)], gendCol = PhenData_Combined$Gender, 
+                         phenCol = PhenData_Combined[, j])
+    out <- list(tempResult, colnames(PhenData_Combined)[j], Sys.time())
+    names(out) <- c("tVal_pVal", "phenotype", "time")
+    return(out)
   }
   
-  # Converting from genotype to MAF using data.table code
-  Gen_Dat <- setDT(Gen_Dat)
-  Gen_Dat <- Gen_Dat[, (colnames(Gen_Dat)[-c(1:2)]) := lapply(.SD, Converter), 
-                     .SDcols = colnames(Gen_Dat)[-c(1:2)]]
-  
-  # Saving data
-  fwrite(Gen_Dat, file = paste0("Partitions/", population, "_Gen_Dat_Clean_Check", p, ".csv"))
-  print(paste0("Partition ", p, " complete"))
-  }
   stopCluster(cl) #Stop executing in parallel
+  
+  saveRDS(results, paste0(getwd(), "/Analysis_Results/", population, "_Mass_Uni_Results_", p, ".rds"))
 }
-
